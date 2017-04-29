fe8754227a8a8082427f8462af3dee23abb6de5b
Tu Nguyen <nogias9x@gmail.com>
2017-04-30 03:04:21 +0700

    doing as demo

--- app/src/main/java/com/example/n50/speedyaladdin/MainActivity.java
+++ app/src/main/java/com/example/n50/speedyaladdin/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.n50.speedyaladdin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.n50.speedyaladdin.Models.GameSurface;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        // Sét đặt giao diện của Activity.
+        this.setContentView(new GameSurface(this));
     }
 }
